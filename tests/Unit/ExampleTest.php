9b9d94a0bc38577bd8c83cbfe0536481c4e16f2f
Roylin <dereklobo89@gmail.com>
2018-08-30 23:12:08 -0400

    learn testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Tests\Unit;
+use App\Post;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -13,8 +14,30 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+    use DatabaseTransactions;
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        //$this->assertTrue(true);
+        
+
+        $first=factory(Post::class)->create();
+        $second=factory(Post::class)->create([
+            'created_at'=>\Carbon\Carbon::now()->subMonth()
+        ]);
+        $posts = Post::archives();
+            //dd($posts);
+        //$this->assertCount(2,$posts);
+        $this->assertEquals([
+        [
+            "year" => $first->created_at->format('Y'),
+            "month" => $first->created_at->format('F'),
+            "published" => 1
+        ],
+        [
+            "year" => $second->created_at->format('Y'),
+            "month" => $second->created_at->format('F'),
+            "published" => 1
+        ]
+        ],$posts);
     }
 }
